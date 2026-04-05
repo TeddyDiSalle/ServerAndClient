fda33a62415a9b9d5c39c71d8f464b0cfad087c1
VincentD274 <v1nc4ntd@gmail.com>
2026-04-05 15:03:17 -0400

    Update Server.py

--- Server.py
+++ Server.py
@@ -10,30 +10,43 @@ serverSocket.bind(('',serverPort))  # binds to all addresses available for local
 #serverSocket.bind(('192.168.0.35',serverPort)) #binds to one specific address for over the air
 serverSocket.listen(5) # server listens for up to 5 clients
 
-client_username = {}
-
-def broadcast():
-
+username_socket = {}
 
 def process_client(client_socket, username):
-    while True:
-        message = client_socket.recv(1024).decode() #receives a message up to 1024 bytes from client, and decodes it
-        if str.startswith(message, "@"):
-            #Feature 2: one to one
-            print();
-        else:
-            #Feature 1: Broadcast
-            broadcast()
+    try:
+        while True:
+            message = client_socket.recv(1024).decode() #receives a message up to 1024 bytes from client, and decodes it
+            if message == "": #if client exits or connection is lost break out of processing loop
+                break
+
+            if str.startswith(message, "@"):
+                #Feature 2: one to one
+                targetUser = message[1, message.find(" ")] #finds and stores target user for direct message
+                if targetUser in username_socket:
+                    username_socket[targetUser].send(message[message.find(" "):].encode())
+                else:
+                    client_socket.send("Target user not found".encode())
+            
+            else:
+                #Feature 1: Broadcast
+                for user, socket in username_socket:
+                    if username != user:
+                     socket.send(message.encode())
+    except:
+        pass #do nothing
+    finally: #post processing and end client thread
+        client_socket.close() #close socket
+        del username_socket[username] #deletes socket and username from dict
 
 
 print('The server is ready to receive')
 while True:   #always welcoming
     connectionSocket, addr = serverSocket.accept()  #Makes a socket and stores address for incoming client
     
-    connectionSocket.send("Enter username".encode()) #Requests client for username
+    connectionSocket.send("Enter username (no spaces)".encode()) #Requests client for username
 
     username = connectionSocket.recv(1024).decode() #Receives username and decodes
-    client_username[username] = connectionSocket #Stores socket and username as a pair in dict
+    username_socket[username] = connectionSocket #Stores socket and username as a pair in dict
 
     client_thead = threading.Thread(target=process_client, args=(connectionSocket, username)) #Starts new thread for client
 
